7e56937e700075d585682719c86394d50c769369
Sabin Dang <sabin@sabindang.com>
2011-09-17 03:46:20 -0700

    initial commit

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+VirtualPagers::Application.config.session_store :cookie_store, :key => '_virtual-pagers_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# VirtualPagers::Application.config.session_store :active_record_store
