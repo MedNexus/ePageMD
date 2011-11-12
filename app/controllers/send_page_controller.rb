3b9d453013093394708b38b3668864acb9ab3a4f
Sabin Dang <sabin@sabindang.com>
2011-11-12 10:18:17 -0800

    friendly form errors on pager form

--- app/controllers/send_page_controller.rb
+++ app/controllers/send_page_controller.rb
@@ -2,14 +2,16 @@ class SendPageController < ApplicationController
   
   def index
     @virtual_pagers = VirtualPager.find(:all, :order => "name")
+    @error = {}
   end
   
   def send_page
     # validate form
-    flash[:error] = "".html_safe
-    flash[:error] += "<li>Must select a pager</li>".html_safe if params[:virtual_pager_id] == ""
-    flash[:error] += "<li>Must enter a message</li>".html_safe if params[:message] == ""
-    if flash[:error] != ""
+    @error = {}
+    @error[:pager] = "<li>Must select a pager</li>".html_safe if params[:virtual_pager_id] == ""
+    @error[:message] = "<li>Must enter a message</li>".html_safe if params[:message] == ""
+    if @error != ""
+      flash[:error] = "Missing fields, please see below"
       @virtual_pagers = VirtualPager.find(:all, :order => "name")
       render :action => 'index'
       return nil
