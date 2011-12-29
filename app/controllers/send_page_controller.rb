7d051f89c292e676596680105ef588e1dad15c8f
Sabin Dang <sabin@sabindang.com>
2011-12-28 23:04:31 -0800

    refactored send page controller to use active model

--- app/controllers/send_page_controller.rb
+++ app/controllers/send_page_controller.rb
@@ -1,30 +1,22 @@
 class SendPageController < ApplicationController
   
   def index
-    @virtual_pagers = VirtualPager.find_all_by_code_level(false)
-    @error = {}
+    @routine_page = RoutinePage.new
   end
   
   def send_page
-    # validate form
-    @error = {}
-    @error[:pager] = "<li>Must select a pager</li>".html_safe if params[:virtual_pager_id] == ""
-    @error[:message] = "<li>Must enter a message</li>".html_safe if params[:message] == ""
-    if @error.size > 0
-      flash.now[:error] = "Missing fields, please see below"
-      @virtual_pagers = VirtualPager.find(:all, :order => "name")
-      render :action => 'index'
-      return nil
-    end
-    
-    vp = VirtualPager.find_by_id(params[:virtual_pager_id])
-    if vp
-      vp.send_page(params[:message]) 
-      flash[:notice] = "Page sent"
+    @routine_page = RoutinePage.new(params[:routine_page])
+    if @routine_page.valid?
+      if @routine_page.send_page
+        flash[:notice] = "Page Sent!"
+        redirect_to :action => 'index'
+      else
+        flash.now[:error] = "ERROR: unable to page an MD"
+        render :action => 'index'
+      end
     else
-      flash[:error] = "Error: unable to send page, please try again later"
+      render :action => 'index'
     end
-    redirect_to :action => 'index'
   end
 
   # controller specifically designed for rapid code blue / rapid response notifications
